0ef8580e26cbffe4a390d10b86ac00dc6ca5d5e8
StevenHCho <stevencho.37@gmail.com>
2017-04-01 04:06:59 +0900

    Testing Tokenizer.java
    1. testGetNextToken(): Getting the next token correctly
    
    2. testTokenizerReader(): Ensuring that the constructor
    Tokenizer(Reader) is working properly
    
    3. testTokenizerString(): Ensuring that the constructor
    Tokenizer(String) is working properly
    
    4. testPeekNextToken():  peeking the next token correctly
    
    5. testPeekNextTokenType(): peeking the type of the next token correctly
    
    6. testIterator(): Ensuring that the iterator's functions are working
    properly
    
    7. testClose(): Getting the next token in a closed reader

--- src/com/esotericsoftware/yamlbeans/tokenizer/Token.java
+++ src/com/esotericsoftware/yamlbeans/tokenizer/Token.java
@@ -18,21 +18,21 @@ package com.esotericsoftware.yamlbeans.tokenizer;
 
 /** @author <a href="mailto:misc@n4te.com">Nathan Sweet</a> */
 public class Token {
-	final static Token DOCUMENT_START = new Token(TokenType.DOCUMENT_START);
-	final static Token DOCUMENT_END = new Token(TokenType.DOCUMENT_END);
-	final static Token BLOCK_MAPPING_START = new Token(TokenType.BLOCK_MAPPING_START);
-	final static Token BLOCK_SEQUENCE_START = new Token(TokenType.BLOCK_SEQUENCE_START);
-	final static Token BLOCK_ENTRY = new Token(TokenType.BLOCK_ENTRY);
-	final static Token BLOCK_END = new Token(TokenType.BLOCK_END);
-	final static Token FLOW_ENTRY = new Token(TokenType.FLOW_ENTRY);
-	final static Token FLOW_MAPPING_END = new Token(TokenType.FLOW_MAPPING_END);
-	final static Token FLOW_MAPPING_START = new Token(TokenType.FLOW_MAPPING_START);
-	final static Token FLOW_SEQUENCE_END = new Token(TokenType.FLOW_SEQUENCE_END);
-	final static Token FLOW_SEQUENCE_START = new Token(TokenType.FLOW_SEQUENCE_START);
-	final static Token KEY = new Token(TokenType.KEY);
-	final static Token VALUE = new Token(TokenType.VALUE);
-	final static Token STREAM_END = new Token(TokenType.STREAM_END);
-	final static Token STREAM_START = new Token(TokenType.STREAM_START);
+	public final static Token DOCUMENT_START = new Token(TokenType.DOCUMENT_START);
+	public final static Token DOCUMENT_END = new Token(TokenType.DOCUMENT_END);
+	public final static Token BLOCK_MAPPING_START = new Token(TokenType.BLOCK_MAPPING_START);
+	public final static Token BLOCK_SEQUENCE_START = new Token(TokenType.BLOCK_SEQUENCE_START);
+	public final static Token BLOCK_ENTRY = new Token(TokenType.BLOCK_ENTRY);
+	public final static Token BLOCK_END = new Token(TokenType.BLOCK_END);
+	public final static Token FLOW_ENTRY = new Token(TokenType.FLOW_ENTRY);
+	public final static Token FLOW_MAPPING_END = new Token(TokenType.FLOW_MAPPING_END);
+	public final static Token FLOW_MAPPING_START = new Token(TokenType.FLOW_MAPPING_START);
+	public final static Token FLOW_SEQUENCE_END = new Token(TokenType.FLOW_SEQUENCE_END);
+	public final static Token FLOW_SEQUENCE_START = new Token(TokenType.FLOW_SEQUENCE_START);
+	public final static Token KEY = new Token(TokenType.KEY);
+	public final static Token VALUE = new Token(TokenType.VALUE);
+	public final static Token STREAM_END = new Token(TokenType.STREAM_END);
+	public final static Token STREAM_START = new Token(TokenType.STREAM_START);
 
 	public final TokenType type;
 
