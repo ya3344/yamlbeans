5ab3b0876b7ea015f94b790ffafad8a8a7882c60
Muntae <kmtae0701@naver.com>
2017-04-02 16:37:16 +0900

    "second commit"

--- src/com/esotericsoftware/yamlbeans/scalar/DateTimeParserTest.java
+++ src/com/esotericsoftware/yamlbeans/scalar/DateTimeParserTest.java
@@ -2,11 +2,11 @@ package com.esotericsoftware.yamlbeans.scalar;
 
 import static org.junit.Assert.*;
 
+import java.text.DateFormat;
 import java.text.FieldPosition;
 import java.text.ParseException;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,44 +14,71 @@ import java.util.Date;
 import org.junit.Test;
 
 import com.esotericsoftware.yamlbeans.scalar.DateTimeParser.Parser;
+import com.esotericsoftware.yamlbeans.scalar.DateTimeParser.SimpleParser;
 
-import junit.framework.Assert;
+import sun.util.resources.CalendarData;
 
 public class DateTimeParserTest {
-
 	private static final String DATEFORMAT_YAML = "yyyy-MM-dd HH:mm:ss";
 	private static final int FORMAT_NONE = -1;
-
+	private SimpleDateFormat outputFormat;
 	DateTimeParser dateTimeParser = new DateTimeParser();
+	private ArrayList<Parser> parsers = new ArrayList<Parser>();
+	
 	@Test
 	public void testDateTimeParser() throws ParseException {
-        Date date = dateTimeParser.parse("2017-03-31 22:06:13");
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        assertEquals(2017, calendar.get(Calendar.YEAR));
-        assertEquals(2, calendar.get(Calendar.MONTH));
-        assertEquals(31, calendar.get(Calendar.DAY_OF_MONTH));
-        assertEquals(22, calendar.get(Calendar.HOUR_OF_DAY));
-        assertEquals(06, calendar.get(Calendar.MINUTE));
-        assertEquals(13, calendar.get(Calendar.SECOND));
+		String s = "1993-07-01 11:22:33";
+		
+		assertEquals(true, parsers.add(new SimpleParser(s)));
+		
+		assertEquals(true, parsers.add(new SimpleParser(DateFormat.FULL, DateFormat.FULL)));
+		assertEquals(true, parsers.add(new SimpleParser(DateFormat.LONG, DateFormat.LONG)));
+		assertEquals(true, parsers.add(new SimpleParser(DateFormat.MEDIUM, DateFormat.MEDIUM)));
+		assertEquals(true, parsers.add(new SimpleParser(DateFormat.SHORT, DateFormat.SHORT)));
+		
+		assertEquals(true, parsers.add(new SimpleParser(DateFormat.FULL, FORMAT_NONE)));
+		assertEquals(true, parsers.add(new SimpleParser(DateFormat.LONG, FORMAT_NONE)));
+		assertEquals(true, parsers.add(new SimpleParser(DateFormat.MEDIUM, FORMAT_NONE)));
+		assertEquals(true, parsers.add(new SimpleParser(DateFormat.SHORT, FORMAT_NONE)));
+		
+		assertEquals(true, parsers.add(new SimpleParser(FORMAT_NONE, DateFormat.FULL)));
+		assertEquals(true, parsers.add(new SimpleParser(FORMAT_NONE, DateFormat.LONG)));
+		assertEquals(true, parsers.add(new SimpleParser(FORMAT_NONE, DateFormat.MEDIUM)));
+		assertEquals(true, parsers.add(new SimpleParser(FORMAT_NONE, DateFormat.SHORT)));
 	}
-
+	
 	@Test
-	public void testParseStringParsePosition() throws ParseException {
-		String text = "7:03 AM";
-		ParsePosition pos = new ParsePosition(0);
-		Date date = dateTimeParser.parse(text, pos);		
-	}
-
-	@Test
-	public void testFormatDateStringBufferFieldPosition() throws ParseException {
-		SimpleDateFormat outputFormat = new SimpleDateFormat();
-		Date date = dateTimeParser.parse("2017-03-31 22:06:13");
-		FieldPosition pos = new FieldPosition(0);
-		StringBuffer buf = new StringBuffer();
+	public void testParseStringParsePosition() {
+		String text1 = "7:03:47 AM PDT";
+		ParsePosition pos1 = new ParsePosition(0);
+		Date date1 = dateTimeParser.parse(text1, pos1);
+		assertEquals(null, date1);
+		
+		String text2 = "7:03:47 AM";
+		ParsePosition pos2 = new ParsePosition(0);
+		Date date2 = dateTimeParser.parse(text2, pos2);
+		assertEquals(null, date2);
+		
+		String text3 = "7:03 AM";
+		ParsePosition pos3 = new ParsePosition(0);
+		Date date3 = dateTimeParser.parse(text3, pos3);
+		assertEquals(null, date3);
+		
+		String text4 = "1993";
+		ParsePosition pos4 = new ParsePosition(0);
+		Date date4 = dateTimeParser.parse(text4, pos4);
 		Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-		outputFormat.format(date, buf, pos);
+        calendar.setTime(date4);
+		assertEquals(1993, Long.parseLong(text4));
 	}
 
+	@Test
+	public final void testFormatDateStringBufferFieldPosition() throws ParseException {
+		SimpleDateFormat outputFormat = new SimpleDateFormat();
+		String s = "1993-07-01 11:22:33";
+		Date date = dateTimeParser.parse(s);
+		FieldPosition pos = new FieldPosition(0);
+		StringBuffer buf = new StringBuffer(0);	
+//		assertEquals(buf.length(), outputFormat.format(date, buf, pos));
+	}
 }
